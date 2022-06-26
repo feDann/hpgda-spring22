f779323bdc64cef6597ab2c901e8725dda1bd3d7
feDann <daniele99@ferrazzo.it>
2022-06-26 20:52:27 +0200

    update run.sh

--- cuda_CONTEST/run.sh
+++ cuda_CONTEST/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Run vector sum;
-bin/b -d -c -n 100000000 -b vec -I 1 -i 30 -t 64;
-bin/b -d -c -n 100000000 -b vec -I 2 -i 30 -t 64;
+# bin/b -d -c -n 100000000 -b vec -I 1 -i 30 -t 64;
+# bin/b -d -c -n 100000000 -b vec -I 2 -i 30 -t 64;
 
 # Run matrix multiplication;
-bin/b -d -c -n 1000 -b mmul -I 1 -i 30 -t 8;
-bin/b -d -c -n 1000 -b mmul -I 2 -i 30 -t 8 -B 14;
+# bin/b -d -c -n 1000 -b mmul -I 1 -i 30 -t 8;
+# bin/b -d -c -n 1000 -b mmul -I 2 -i 30 -t 8 -B 14;
 
 #run PPR
-/bin/b -c -b ppr -t 512 -I 4 -i 100 -g ./data/wikipedia-20070206.mtx
+bin/b -c -b ppr -t 512 -I 4 -i 100 -e 1e-3 -g ./data/wikipedia-20070206.mtx
